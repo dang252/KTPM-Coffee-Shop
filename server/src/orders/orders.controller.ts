a3c684044924410b40d55a1530fb144bddbb8b87
20120050 <dang_252@yahoo.com.vn>
2023-12-31 02:29:05 +0700

    update get history by userid

--- server/src/orders/orders.controller.ts
+++ server/src/orders/orders.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 
 import { OrdersService } from './orders.service';
 import { createOrderRequest, getOrderResponse } from './dto/order.dto';
@@ -7,6 +7,11 @@ import { createOrderRequest, getOrderResponse } from './dto/order.dto';
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) { }
+
+  @Get("/history")
+  async getOrderByUserId(@Query() query: any) {
+    return this.ordersService.getOrderByUserId(query.userId)
+  }
 
   @Post("/create")
   async createOrder(@Body() req: createOrderRequest) {
